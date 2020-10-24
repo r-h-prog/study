b00c8b57cf01cde4a7df28320be3043e2fe89e4e
r-h-prog <ryota0824break>
2020-10-24 09:39:58 +0900

    1024

---
+++ AR_VR/ARビギナー講座/note.md
@@ -0,0 +1,19 @@
+# まとめ
+
+## Unity
+
+### RigidBody
+
+#### Mass
+
+重さ
+
+## Drag
+
+摩擦係数
+
+## Use Grabity
+
+重力
+
+
---
+++ JS/travasty-20projects/movie-seat-booking/script.js
@@ -0,0 +1,25 @@
+const container = dosument.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatscount = selectedSeats.length;
+
+  count.innerText = selectedSeatscount;
+  total.innerText = selectedSeatscount * ticketPrice;
+
+}
+
+container.addEventListner('click', (e) => {
+  if (e.target.classList.contains('.seat') && !e.target.classList.contains('.occupied')) {
+    e.target.classList.toggle('.selected');
+
+    updateSelectedCount();
+  }
+})
